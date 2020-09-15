01bb499662d34e5964e8df8a972efed9b2002474
Roberto Acosta <x.rojo@hotmail.com>
2020-09-15 01:11:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,13 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
+
+def player1
+  player1 = "Jay-Z"
+def player2
+  player2 = "Beyonce"
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
